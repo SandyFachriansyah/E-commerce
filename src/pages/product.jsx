aa1b85ffeff9fb6e811c8f013b77c114cafdf482
SandyFachriansyah <fachriansyahsandy@gmail.com>
2023-11-02 01:04:27 +0700

    Terbaru

--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -2,16 +2,22 @@ import React, { useContext } from 'react'
 import  {ShopContext}  from '../context/ShopContext'
 import { useParams } from 'react-router-dom';
 import Breadcrum from '../component/Breadcrums/Breadcrum'
+import ProductDisplay from '../component/ProductDisplay/ProductDisplay';
+
+
 
 const Product = () => {
   
-  const {all_product} = useContext(ShopContext);
+  const  all_product  = useContext(ShopContext);
   const {productID} = useParams()
   const product = all_product.find((e) => e.id === Number(productID))
+  console.log("Product Data:", productID);
+
 
   return (
     <div>
       <Breadcrum product={product}/>
+      <ProductDisplay product={product}/>
     </div>
   )
 }
